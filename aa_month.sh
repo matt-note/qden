66f5aca81c0140c972c4cd9145c1fa53b6268f98
matt-note <sugarandsalt89@gmail.com>
2020-11-07 23:15:59 +0900

    23th commit

--- aa_month.sh
+++ aa_month.sh
@@ -4,19 +4,20 @@
 START_YM=20200616
 END_YM=`date +%Y%m%d`
 
-# 新規ファイルは > で作成できるので、過去のファイルは削除。
+# 新規ファイルは >>で作成できるので、過去のファイルは削除。
 rm month.csv
 
 TEMP_YM=$START_YM
 while [ 1 ] ; do
 
   # END_YM分まで処理したら終わり
-  if [ $TEMP_YM = $END_YM ] ; then
+  if [ ${TEMP_YM:0:6} = ${END_YM:0:6} ] ; then
     break
   fi
 
   # 日付をファイルに書き込み
-  echo `date -d "$TEMP_YM 1 month" "+%Y-%m"` > month.csv
+  # > だけだと無限ループになる。
+  echo `date -d "$TEMP_YM 1 month" "+%Y-%m"` >> month.csv
 
   # 日付をインクリメント
   TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y%m%d"`
