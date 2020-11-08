3f9ca984f730c5379f999d6755b713afa659bd37
matt-note <sugarandsalt89@gmail.com>
2020-11-08 14:48:35 +0900

    37th commit

--- aa_month.sh
+++ aa_month.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # MacでGNUのdateコマンドを使うには、gdateコマンドとなる。もどかしい。。
-START_YM=20200816
-END_YM=`date +%Y%m%d`
+START_YM=2020-09-16
+END_YM=`date +%Y-%m-%d`
 
 TEMP_YM=$START_YM
 while [ 1 ] ; do
 
   # yyyymmを比較して、END_YM分まで処理したら終わり。それまで月を1加算していく。
-  if [ ${TEMP_YM:0:6} = ${END_YM:0:6} ] ; then
+  if [ ${TEMP_YM:0:7} = ${END_YM:0:7} ] ; then
     break
   fi
 
   # 一旦、ファイル作成を確認
-  echo "hoge" > data/${TEMP_YM:0:4}-${TEMP_YM:5:6}.json
+  echo "hoge" > data/${TEMP_YM:0:7}.json
 
-  # 日付をインクリメント
-  TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y%m%d"`
+  # 日付をインクリメント 20201108
+  TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y-%m-%d"`
 done
