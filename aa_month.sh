72b3090958ec2af5638f682e347fa54c52151f3e
matt-note <sugarandsalt89@gmail.com>
2020-11-08 16:29:26 +0900

    42th commit

--- aa_month.sh
+++ aa_month.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 開始日付と実行時日付を設定
-START_YM=2019-09-16
+START_YM=2020-09-16
 END_YM=`date +%Y-%m-%d`
 
 TEMP_YM=$START_YM
@@ -12,12 +12,15 @@ while [ 1 ] ; do
     break
   fi
 
+  LIMIT_YM=`date -d "$TEMP_YM 1 month" "+%Y-%m"`
+
   # 一旦、ファイル作成を確認
+  # 欲しい期間のデータを取得できるか確認
   curl -G \
-    --data-urlencode "query=created:>2020-10-01 created:<2020-11 stocks:>262" \
+    --data-urlencode "query=created:>${TEMP_YM} created:<${LIMIT_YM} stocks:>262" \
     --data-urlencode "page=1" \
     --data-urlencode "per_page=100" \
-  -H 'Authorization: Bearer c32dd88c4bdff501c8ab63f2b72b7b7547a1a44b' 'https://qiita.com/api/v2/items' | \
+  -H 'Authorization: Bearer ${{ secrets.QIITA_TOKEN }}' 'https://qiita.com/api/v2/items' | \
   jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > data/${TEMP_YM:0:7}.json
 
   # 月を1加算
