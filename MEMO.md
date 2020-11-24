8b05bc0ca1b309ab80a3f2c01654ea0ee121ea02
matt-note <sugarandsalt89@gmail.com>
2020-11-24 21:27:43 +0900

    50th commit

--- MEMO.md
+++ MEMO.md
@@ -45,13 +45,6 @@ curl -G \
   --data-urlencode "page=1" \
   --data-urlencode "per_page=100" \
 -H 'Authorization: Bearer c32dd88c4bdff501c8ab63f2b72b7b7547a1a44b' 'https://qiita.com/api/v2/items' | \
-jq '. | map({ title: .title, url: .url, likes_count: .likes_count, created_at: .created_at, updated_at: .updated_at, id: .user.id}) | sort_by(.likes_count) | reverse' > ranking.json
-
-curl -G \
-  --data-urlencode "query=created:>2020-10-01 created:<2020-11 stocks:>262" \
-  --data-urlencode "page=1" \
-  --data-urlencode "per_page=100" \
--H 'Authorization: Bearer c32dd88c4bdff501c8ab63f2b72b7b7547a1a44b' 'https://qiita.com/api/v2/items' | \
 jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > ranking.json
 
 ## ページネーションの仕様
--- aa_month.sh
+++ aa_month.sh
@@ -12,9 +12,6 @@ while [ 1 ] ; do
     break
   fi
 
-  # debug
-  echo $TEMP_YM
-
   # 一旦、ファイル作成を確認
   # 欲しい期間のデータを取得できるか確認
   curl -G \
@@ -23,6 +20,13 @@ while [ 1 ] ; do
     --data-urlencode "per_page=100" \
   -H 'Authorization: Bearer ${{ secrets.QIITA_TOKEN }}' 'https://qiita.com/api/v2/items' | \
   jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > data/${TEMP_YM:0:7}.json
+
+  curl -G \
+    --data-urlencode "query=created:>2020-10-01 created:<2020-11 stocks:>262" \
+    --data-urlencode "page=1" \
+    --data-urlencode "per_page=100" \
+  -H 'Authorization: Bearer c32dd88c4bdff501c8ab63f2b72b7b7547a1a44b' 'https://qiita.com/api/v2/items' | \
+  jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > ranking.json
 
   # 月を1加算
   TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y-%m-%d"`
