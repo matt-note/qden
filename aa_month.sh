02f96aeedfc8216a69619523a917ec305ff712a9
matt-note <sugarandsalt89@gmail.com>
2020-12-02 01:08:41 +0900

    98th commit

--- aa_month.sh
+++ aa_month.sh
@@ -19,7 +19,7 @@ while [ 1 ] ; do
 
   # データ取得。Qiitaでは「以上」の検索ができないっぽいので、「同日+より多い日付」で検索。
   curl -G \
-    --data-urlencode "created:$TEMP_YM created:>$TEMP_YM created:<$(date -d "$TEMP_YM 1 month" "+%Y-%m") stocks:>261" \
+    --data-urlencode "created:>$TEMP_YM created:<$(date -d "$TEMP_YM 1 month" "+%Y-%m") stocks:>261" \
     --data-urlencode "page=1" \
     --data-urlencode "per_page=100" \
     --silent \
