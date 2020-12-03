89e553517b812d5f04f73566dc68ec27e8fa343c
matt-note <sugarandsalt89@gmail.com>
2020-12-04 00:47:17 +0900

    109th commi

--- aa_month.sh
+++ aa_month.sh
@@ -17,7 +17,7 @@ while [ 1 ] ; do
   TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y-%m-%d"`
 
   # echo "*** "$TEMP_YM" ***"
-  echo $(date -d "$TEMP_YM -1 day" "+Y-%m-%d")
+  date -d "$TEMP_YM -1 day" "+Y-%m-%d"
 
   # 一旦、ファイル作成を確認
   # 欲しい期間のデータを取得できるか確認
