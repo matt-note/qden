7c81eed54334d05acc10655d67fac7314eae32e3
matt-note <sugarandsalt89@gmail.com>
2020-12-04 00:26:11 +0900

    108th commit

--- aa_month.sh
+++ aa_month.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # 開始日付と実行時日付を設定
@@ -12,16 +13,19 @@ while [ 1 ] ; do
     break
   fi
 
-  # 月を1加算して月末の日付となるように設定
-  TEMP_YM=`date -d "$TEMP_YM + 1 month"`
+  # 月を1加算
+  TEMP_YM=`date -d "$TEMP_YM 1 month" "+%Y-%m-%d"`
 
-  # データ取得。Qiitaは「以上」の検索ができないっぽい。
+  echo "*** "$TEMP_YM" ***"
+
+  # 一旦、ファイル作成を確認
+  # 欲しい期間のデータを取得できるか確認
   curl -G \
     --data-urlencode "query=created:>$TEMP_YM created:<$(date -d "$TEMP_YM 1 month" "+%Y-%m") stocks:>261" \
     --data-urlencode "page=1" \
     --data-urlencode "per_page=100" \
     --silent \
   -H 'Authorization: Bearer '$QIITA_TOKEN 'https://qiita.com/api/v2/items' | \
-  jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > data/$(date -d "$TEMP_YM" "+%Y-%m").json
+  jq '. | map({ title: .title?, url: .url?, likes_count: .likes_count?, created_at: .created_at?, updated_at: .updated_at?, id: .user.id?}) | sort_by(.likes_count) | reverse' > data/${TEMP_YM:0:7}.json
 
 done
